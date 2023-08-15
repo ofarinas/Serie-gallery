326209e04feabf45a64737dda7d4dd5d5556ae7c
Osvaldo <ofarinas12@gmail.com>
2023-08-15 11:42:59 +0200

    add meterialize ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a class="waves-effect waves-light btn">button</a>
