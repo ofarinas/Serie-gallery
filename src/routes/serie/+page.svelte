5dada7dcaf5c7718b2e727d9c5a02ad112c0f5e4
Osvaldo <ofarinas12@gmail.com>
2023-08-17 15:20:56 +0200

    fix loading problem

--- src/routes/serie/+page.svelte
+++ src/routes/serie/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Load from '$lib/components/Load.svelte';
 	import type { Serie } from '$lib/components/serie.model';
 	import { serieCatalogStore } from '$lib/components/serieCatalog.store';
 	import { onMount } from 'svelte';
@@ -12,37 +13,42 @@
 	});
 
 	$: serieId = $page.url.searchParams.get('serieId');
+	$: loading = serieCatalogStore.loading;
 </script>
 
-<div class="container">
-	<h3>Serie: {serie?.SeriesName}</h3>
-	<h6>{serie?.Episodes.length} Episodes</h6>
-	<div class="back-link">
-		<a href={'.'}>Back</a>
-	</div>
+{#if $loading &&!serie}
+	<Load />
+{:else}
+	<div class="container">
+		<h3>Serie: {serie?.SeriesName}</h3>
+		<h6>{serie?.Episodes.length} Episodes</h6>
+		<div class="back-link">
+			<a href={'.'}>Back</a>
+		</div>
 
-	<ul class="collapsible">
-		{#each serie?.Episodes ?? [] as episode}
-			<li>
-				<div class="collapsible-header">{episode.EpisodeName}</div>
-				<div class="collapsible-body">
-					<h6>Overview</h6>
-					<div>
-						{episode.Overview}
+		<ul class="collapsible">
+			{#each serie?.Episodes ?? [] as episode}
+				<li>
+					<div class="collapsible-header">{episode.EpisodeName}</div>
+					<div class="collapsible-body">
+						<h6>Overview</h6>
+						<div>
+							{episode.Overview}
+						</div>
+						<h6>Director</h6>
+						<div>
+							{episode.Director ?? '-'}
+						</div>
+						<h6>Rating</h6>
+						<div>
+							{episode.Rating}
+						</div>
 					</div>
-					<h6>Director</h6>
-					<div>
-						{episode.Director ?? '-'}
-					</div>
-					<h6>Rating</h6>
-					<div>
-						{episode.Rating}
-					</div>
-				</div>
-			</li>
-		{/each}
-	</ul>
-</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
+{/if}
 
 <style>
 	.container {
